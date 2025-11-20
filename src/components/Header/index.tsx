c6f5b8c5ac26f41462f564f8ede8c6b39f9fe8bb
Abderrahmane Meziani <127858453+abderrM@users.noreply.github.com>
2025-11-20 10:57:24 +0100

    nothing

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -107,17 +107,17 @@ const Header = () => {
                   <ul className="block lg:flex lg:space-x-12">
                     {menuData.map((menuItem, index) => (
                       <li key={index} className="group relative">
-                        {menuItem.path ? (
-                          <Link
-                            href={menuItem.path}
-                            className={`flex py-2 text-base lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 ${
-                              usePathName === menuItem.path
-                                ? "text-primary dark:text-white"
-                                : "text-dark hover:text-primary dark:text-white/70 dark:hover:text-white"
-                            }`}
-                          >
-                            {menuItem.title}
-                          </Link>
+                          {menuItem.path ? (
+                            <Link
+                              href={menuItem.path}
+                              className={`flex py-2 text-base lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 transition-colors duration-300 ${
+                                usePathName === menuItem.path
+                                  ? "text-primary dark:text-white"
+                                  : "text-dark hover:text-primary dark:text-white/70 dark:hover:text-white"
+                              }`}
+                            >
+                              {menuItem.title}
+                            </Link>
                         ) : (
                           <>
                             <p
