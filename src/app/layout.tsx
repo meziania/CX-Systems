c1b853209f425a1ab26abdc3596a590c28c8d9dc
Abderrahmane Meziani <127858453+abderrM@users.noreply.github.com>
2025-11-22 15:57:26 +0100

    push

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
-"use client";
-
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import ScrollToTop from "@/components/ScrollToTop";
-import { Providers } from "./providers";
+import { Metadata } from "next";
 import { Inter } from "next/font/google";
+import ClientLayout from "./layout-client";
 import "../styles/index.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Customer Experience Systems",
+  description: "CX System offers innovative solutions to optimize your company's customer experience. Transform your customer interactions with our advanced tools.",
+};
 
 export default function RootLayout({
   children,
@@ -16,22 +17,8 @@ export default function RootLayout({
 }) {
   return (
     <html suppressHydrationWarning lang="fr">
-      <head>
-        <title>Customer Experience Systems</title>
-        <meta name="description" content="CX System offers innovative solutions to optimize your company's customer experience. Transform your customer interactions with our advanced tools." />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon.png" />
-        <link rel="icon" type="image/png" sizes="180x180" href="/images/favicon.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon.png" />
-      </head>
-
       <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}>
-        <Providers>
-          <Header />
-          {children}
-          <Footer />
-          <ScrollToTop />
-        </Providers>
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,6 @@ import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
 import Testimonials from "@/components/Testimonials";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "CX System - Revolutionize Your Customer Experience",
-  description: "CX System offers innovative solutions to optimize your company's customer experience. Transform your customer interactions with our advanced tools.",
-  // other metadata
-};
 
 export default function Home() {
   return (
